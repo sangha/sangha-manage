6d54a2841c0169ce7905afebdd6048126728aa40
Christian Muehlhaeuser <muesli@gmail.com>
2018-05-09 11:24:03 +0200

    Add newline-br helper

---
+++ app/helpers/newline-br.js
@@ -0,0 +1,10 @@
+import { htmlSafe } from '@ember/template';
+import Helper from '@ember/component/helper';
+
+export default Helper.extend({
+  compute(params, hash) {
+    var breakTag = '<br />';
+    let t = hash.text;
+    return new htmlSafe(t.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2'));
+  }
+});
