99627418f81870921d496b2d0ad260594cf419c7
Christian Muehlhaeuser <muesli@gmail.com>
2018-06-07 14:31:22 +0200

    Add array & isodate transforms

---
+++ app/transforms/array.js
@@ -0,0 +1,23 @@
+import { typeOf } from '@ember/utils';
+import DS from 'ember-data';
+
+export default DS.Transform.extend({
+  deserialize(serialized) {
+    return (typeOf(serialized) == "array") ?
+      serialized :
+      [];
+  },
+
+  serialize(deserialized) {
+    var type = typeOf(deserialized);
+    if (type == 'array') {
+      return deserialized
+    } else if (type == 'string') {
+      return deserialized.split(',').map(function (item) {
+        return jQuery.trim(item);
+      });
+    } else {
+      return [];
+    }
+  }
+});
---
+++ app/transforms/isodate.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+
+export default DS.Transform.extend({
+  deserialize: function (serialized) {
+    // default date in Go
+    if (serialized === '0001-01-01T00:00:00Z') {
+      return undefined;
+    }
+
+    return new Date(Date.parse(serialized));
+  },
+
+  serialize: function (date) {
+    if (date instanceof Date) {
+      return date.toISOString();
+    } else {
+      return undefined;
+    }
+  }
+});
