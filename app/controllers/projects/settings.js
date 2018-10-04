bcc97037a2fda578e3062a2db82641731b5a4d27
Christian Muehlhaeuser <muesli@gmail.com>
2018-10-04 02:21:40 +0200

    Use brace expansion

--- app/controllers/projects/settings.js
+++ app/controllers/projects/settings.js
@@ -2,7 +2,7 @@ import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  dialogNotValid: computed('project.name', 'project.summary', 'project.website', 'project.license', 'project.repository', function () {
+  dialogNotValid: computed('project.{name,summary,website,license,repository}', function () {
     var p = this.project;
     return p.get('name').length == 0 || p.get('summary').length == 0 ||
       p.get('website').length == 0 || p.get('license').length == 0 ||
