615883c474dba5bbc8dc25d91d98ee9f68c0f92f
Christian Muehlhaeuser <muesli@gmail.com>
2018-10-06 10:49:11 +0200

    Return an empty string when newline-br's text is undefined

--- app/helpers/newline-br.js
+++ app/helpers/newline-br.js
@@ -3,8 +3,11 @@ import Helper from '@ember/component/helper';
 
 export default Helper.extend({
   compute(params, hash) {
-    var breakTag = '<br />';
+    var breakTag = '<br>';
     let t = hash.text;
+    if (t == undefined) {
+      return "";
+    }
     return new htmlSafe(t.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2'));
   }
 });
