8d2bdfe30242f3ccb45195690ea2f6735984c9c5
Christian Muehlhaeuser <muesli@gmail.com>
2018-10-10 18:07:29 +0200

    Add integration tests

---
+++ tests/integration/components/transaction-item-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | transaction item', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+
+    await render(hbs`{{transaction-item}}`);
+
+    assert.dom('*').hasText('assignment_return External payment 0.00 €');
+  });
+});
